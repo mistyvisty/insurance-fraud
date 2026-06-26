a2fb18442a716ede8fe7e4ddafe071c803f21858
PREETI BHARDWAJ <bhardwajpreeti357@gmail.com>
2026-06-26 21:32:20 +0530

    Update db_connect.py

--- db_connect.py
+++ db_connect.py
@@ -3,11 +3,8 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 def get_engine():
-    host = os.getenv("DB_HOST", "localhost")
-    db   = os.getenv("DB_NAME", "insurance_fraud")
-    user = os.getenv("DB_USER", "preeti")
-    pw   = os.getenv("DB_PASSWORD", "fraud123")
-    return create_engine(f"postgresql://{user}:{pw}@{host}:5432/{db}")
+    url = os.getenv("DATABASE_URL")
+    return create_engine(url)
 
 def run_query(sql: str) -> pd.DataFrame:
     with get_engine().connect() as conn:
